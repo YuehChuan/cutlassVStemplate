3e634f257394b82aab0ea3e1083a856e0d322c7e
Johnson <schwarmcyc@hotmail.com>
2023-08-07 16:16:38 +0800

    init commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+int main(int argc, char* argv[])
+{
+    cout << "Welcome to Babylon 5!!! " << endl;
+
+    return 0;
+}
